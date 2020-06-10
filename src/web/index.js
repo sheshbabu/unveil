a587af928e65234ae77dd7d78ccc9cf4713512d0
sheshbabu <sheshbabu@gmail.com>
2020-06-10 21:42:49 +0800

    Refactored to use web components

--- src/web/index.js
+++ src/web/index.js
@@ -1,62 +1,3 @@
-async function init() {
-  bindEvents();
-  await fetchFlags();
-}
-
-function bindEvents() {
-  $("#add-flag-btn.ui.primary.button").click(() => {
-    $(".ui.modal form input").val("");
-    $(".ui.modal").modal("show");
-  });
-  $("#save-flag-btn.ui.primary.button").click(async () => {
-    const name = $("form.ui.form")[0].name.value;
-    await request("/api/flags", "POST", { name });
-    $(".ui.modal").modal("hide");
-    await fetchFlags();
-  });
-}
-
-async function fetchFlags() {
-  $(".vexil-home-empty-content-container").addClass("vexil-hidden");
-  $(".vexil-home-loaded-content-container").addClass("vexil-hidden");
-  $(".vexil-home-loading-content-container").removeClass("vexil-hidden");
-
-  const flags = await request("/api/flags");
-
-  $(".vexil-home-loading-content-container").addClass("vexil-hidden");
-
-  if (flags.length === 0) {
-    $(".vexil-home-empty-content-container").removeClass("vexil-hidden");
-    return;
-  }
-
-  const rows = flags.reduce((acc, flag) => {
-    const statusText = flag.is_enabled ? "On" : "Off";
-    const checked = flag.is_enabled ? "checked" : "";
-    return (acc += `
-          <tr>
-            <td>${flag.name}</td>
-            <td>
-              <div class="ui toggle checkbox" data-name="${flag.name}">
-                <input type="checkbox" tabindex="0" class="hidden" ${checked}>
-                <label>${statusText}</label>
-              </div>
-            </td>
-          </tr>
-          `);
-  }, "");
-
-  $(".vexil-home-loaded-content-container tbody").html(rows);
-  $(".ui.checkbox")
-    .checkbox()
-    .change(async function () {
-      const name = $(this).data("name");
-      const is_enabled = $(this).hasClass("checked");
-      await request("/api/flags", "PUT", { name, is_enabled });
-    });
-  $(".vexil-home-loaded-content-container").removeClass("vexil-hidden");
-}
-
 async function request(url, method = "GET", body) {
   const response = await fetch(url, {
     method,
@@ -72,3 +13,158 @@ async function request(url, method = "GET", body) {
     throw new Error(response.statusText);
   }
 }
+
+class VxApp extends HTMLElement {
+  static get observedAttributes() {
+    return ["loading", "flags"];
+  }
+
+  connectedCallback() {
+    this.fetchFlags();
+  }
+
+  attributeChangedCallback() {
+    this.render();
+  }
+
+  async fetchFlags() {
+    this.loading = true;
+    try {
+      this.flags = await request("/api/flags");
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  openModal() {
+    $(".ui.modal form input").val("");
+    $(".ui.modal").modal("show");
+  }
+
+  async closeModal() {
+    const name = $("form.ui.form")[0].name.value;
+    await request("/api/flags", "POST", { name });
+    $(".ui.modal").modal("hide");
+    await this.fetchFlags();
+  }
+
+  render() {
+    let content = `<vx-content flags='${JSON.stringify(this.flags)}'></vx-content>`;
+
+    if (this.loading) {
+      content = "<vx-loader></vx-loader>";
+    }
+
+    if (this.flags && this.flags.length === 0) {
+      content = "<vx-empty-content></vx-empty-content>";
+    }
+
+    this.innerHTML = `
+      <div class="ui text container">
+        <vx-header></vx-header>
+        ${content}
+        <vx-modal></vx-modal>
+      </div>
+    `;
+
+    $("#add-flag-btn.ui.primary.button").click(() => {
+      this.openModal();
+    });
+
+    $("#save-flag-btn.ui.primary.button").click(() => {
+      this.closeModal();
+    });
+  }
+
+  get loading() {
+    return JSON.parse(this.getAttribute("loading"));
+  }
+
+  set loading(v) {
+    this.setAttribute("loading", JSON.stringify(v));
+  }
+
+  get flags() {
+    return JSON.parse(this.getAttribute("flags"));
+  }
+
+  set flags(v) {
+    this.setAttribute("flags", JSON.stringify(v));
+  }
+}
+
+class VxHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = document.getElementById("vx-header").innerHTML;
+  }
+}
+
+class VxLoader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = document.getElementById("vx-loader").innerHTML;
+  }
+}
+
+class VxEmptyContent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = document.getElementById("vx-empty-content").innerHTML;
+  }
+}
+
+class VxContent extends HTMLElement {
+  static get observedAttributes() {
+    return ["flags"];
+  }
+
+  attributeChangedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = document.getElementById("vx-content").innerHTML;
+
+    const rows = this.flags.reduce((acc, flag) => {
+      return (acc += `
+        <tr>
+          <td>${flag.name}</td>
+          <td>
+            <div class="ui toggle checkbox" data-name="${flag.name}">
+              <input type="checkbox" tabindex="0" class="hidden" ${flag.is_enabled ? "checked" : ""}>
+              <label>${flag.is_enabled ? "On" : "Off"}</label>
+            </div>
+          </td>
+        </tr>
+      `);
+    }, "");
+
+    this.querySelector("tbody").innerHTML = rows;
+
+    $(".ui.checkbox")
+      .checkbox()
+      .change(async function () {
+        const name = $(this).data("name");
+        const is_enabled = $(this).hasClass("checked");
+        await request("/api/flags", "PUT", { name, is_enabled });
+        $(this)
+          .find("label")
+          .html(is_enabled ? "On" : "Off");
+      });
+  }
+
+  get flags() {
+    return JSON.parse(this.getAttribute("flags"));
+  }
+}
+
+class VxModal extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = document.getElementById("vx-modal").innerHTML;
+  }
+}
+
+window.customElements.define("vx-app", VxApp);
+window.customElements.define("vx-header", VxHeader);
+window.customElements.define("vx-loader", VxLoader);
+window.customElements.define("vx-empty-content", VxEmptyContent);
+window.customElements.define("vx-content", VxContent);
+window.customElements.define("vx-modal", VxModal);
