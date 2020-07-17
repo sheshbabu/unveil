73eac2cb1969437a6d5085b94e3b27c5cb144bac
sheshbabu <sheshbabu@gmail.com>
2020-07-17 19:02:52 +0800

    Added error handling

--- src/api.rs
+++ src/api.rs
@@ -1,25 +1,35 @@
-#[path = "./model.rs"]
-mod model;
-
+use super::model;
+use super::types::{CreateFlag, UpdateFlag};
 use actix_web::web::{Data, Json};
 use actix_web::{HttpResponse, Responder};
-use model::{CreateFlag, UpdateFlag};
 use sqlx::PgPool;
 
 pub async fn get_all_flags(pool: Data<PgPool>) -> impl Responder {
-  let result = model::get_all_flags(pool.get_ref()).await;
-
-  HttpResponse::Ok().json(result)
+  model::get_all_flags(pool.get_ref())
+    .await
+    .map(|flags| HttpResponse::Ok().json(flags))
+    .map_err(|error| {
+      log::error!("Error {:?}", error);
+      HttpResponse::InternalServerError()
+    })
 }
 
 pub async fn create_flag(flag: Json<CreateFlag>, pool: Data<PgPool>) -> impl Responder {
-  let result = model::create_flag(pool.get_ref(), flag).await;
-
-  HttpResponse::Ok().json(result)
+  model::create_flag(pool.get_ref(), flag)
+    .await
+    .map(|_| HttpResponse::Ok())
+    .map_err(|error| {
+      log::error!("Error {:?}", error);
+      HttpResponse::InternalServerError()
+    })
 }
 
 pub async fn update_flag(flag: Json<UpdateFlag>, pool: Data<PgPool>) -> impl Responder {
-  let result = model::update_flag(pool.get_ref(), flag).await;
-
-  HttpResponse::Ok().json(result)
+  model::update_flag(pool.get_ref(), flag)
+    .await
+    .map(|_| HttpResponse::Ok())
+    .map_err(|error| {
+      log::error!("Error {:?}", error);
+      HttpResponse::InternalServerError()
+    })
 }
